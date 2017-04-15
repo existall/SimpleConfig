52cfb960e61b6392294c0f9c2438234f760dd1f6
Guy <guy.lud@gmail.com>
2017-04-15 19:54:28 +0300

    remove current value from c`tor

--- src/Core/ExistAll.SimpleConfig/ConfigBindingContext.cs
+++ src/Core/ExistAll.SimpleConfig/ConfigBindingContext.cs
@@ -8,14 +8,14 @@ namespace ExistAll.SimpleConfig
 		public string Key { get; }
 		public string CurrentValue { get; internal set; }
 
-		public ConfigBindingContext(string section, string key, string value)
+		public ConfigBindingContext(string section, string key)
 		{
 			if (section == null) throw new ArgumentNullException(nameof(section));
 			if (key == null) throw new ArgumentNullException(nameof(key));
 
 			Section = section;
 			Key = key;
-			CurrentValue = value;
+			CurrentValue = null;
 		}
 	}
 }
