88e3a87ebd40d9289b049f116cb2165710ff2708
Guy <guy.lud@gmail.com>
2017-08-09 23:42:41 +0300

    start impl for EnviromentVariable Binder as default

---
+++ src/Tests/ExistAll.SimpleConfig.UnitTests/Binders/EnviromentVariableBinderTests.cs
@@ -0,0 +1,32 @@
+﻿using ExistAll.SimpleConfig.Binder;
+using Xunit;
+
+namespace ExistAll.SimpleConfig.UnitTests.Binders
+{
+	public class EnviromentVariableBinderTests
+	{
+		[Fact]
+		public void TryGetValue_WhereVariableNotPresent_ShouldReturnFalse()
+		{
+			var sut = new EnvironmentVariablesBinder();
+			string value;
+
+			var result = sut.TryGetValue(new ConfigBindingContext("section", "key"), out value);
+			
+			Assert.False(result);
+			Assert.Null(value);
+		}
+		
+		[Fact]
+		public void TryGetValue_WhereVariablePresent_ShouldReturnTrue()
+		{
+			var sut = new EnvironmentVariablesBinder();
+			string value;
+
+			var result = sut.TryGetValue(new ConfigBindingContext("section", "key"), out value);
+			
+			Assert.False(result);
+			Assert.Null(value);
+		}
+	}
+}
