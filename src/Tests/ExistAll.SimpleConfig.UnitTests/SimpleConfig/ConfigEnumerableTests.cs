47d27847d93158f77092e626c40512d8897b36b2
Guy <guy.lud@gmail.com>
2018-04-15 09:01:36 +0300

    Bunp version and add more tetsts for IEnumerable

--- src/Tests/ExistAll.SimpleConfig.UnitTests/SimpleConfig/ConfigEnumerableTests.cs
+++ src/Tests/ExistAll.SimpleConfig.UnitTests/SimpleConfig/ConfigEnumerableTests.cs
@@ -6,6 +6,11 @@ namespace ExistAll.SimpleConfig.UnitTests.SimpleConfig
 {
 	public class ConfigEnumerableTests
 	{
+		private const int N12 = 12;
+		private const int N3 = 3;
+		private const int N4 = 4;
+		private const int N103 = 103;
+
 		[Fact]
 		public void Build_WhereVariableHasValue_ShouldSetProperty()
 		{
@@ -15,15 +20,19 @@ namespace ExistAll.SimpleConfig.UnitTests.SimpleConfig
 
 			var enumerable = result.GetConfig<IEnumerableInterface>();
 
-			Assert.Equal(enumerable.Values.Count(), 5);
+			Assert.Equal(enumerable.Values.Count(), 4);
+
+			Assert.Contains(N12, enumerable.Values);
+			Assert.Contains(N3, enumerable.Values);
+			Assert.Contains(N4, enumerable.Values);
+			Assert.Contains(N103, enumerable.Values);
 		}
 
 		[ConfigSection]
 		public interface IEnumerableInterface
 		{
-			[DefaultValue(12,3,4,5,6)]
+			[DefaultValue(N12, N3, N4, N103)]
 			IEnumerable<int> Values { get; set; }
 		}
-
 	}
 }
