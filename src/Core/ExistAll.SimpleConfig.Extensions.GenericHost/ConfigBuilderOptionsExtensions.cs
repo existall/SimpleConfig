60c41dee3a0b897ebe38d3b5ecc784e87ef4f402
guyl <guy.ludvig@credifi.com>
2019-07-16 22:29:19 +0300

    Fix extensions method return type

--- src/Core/ExistAll.SimpleConfig.Extensions.GenericHost/ConfigBuilderOptionsExtensions.cs
+++ src/Core/ExistAll.SimpleConfig.Extensions.GenericHost/ConfigBuilderOptionsExtensions.cs
@@ -21,58 +21,58 @@ namespace ExistAll.SimpleConfig.Extensions.GenericHost
             return target;
         }
         
-        public static IConfigBuilderOptions SetConfigSuffix(this ConfigBuilderOptions target, string suffix)
+        public static IConfigBuilderOptions SetConfigSuffix(this IConfigBuilderOptions target, string suffix)
         {
             target.SetupOptions(x => x.ConfigSuffix = suffix);
             return target;
         }
 
-        public static IConfigBuilderOptions SetArraySplitDelimiter(this ConfigBuilderOptions target, string arrayDelimiter)
+        public static IConfigBuilderOptions SetArraySplitDelimiter(this IConfigBuilderOptions target, string arrayDelimiter)
         {
             if (arrayDelimiter == null) throw new ArgumentNullException(nameof(arrayDelimiter));
             target.SetupOptions(x => x.ArraySplitDelimiter = arrayDelimiter);
             return target;
         }
 
-        public static IConfigBuilderOptions SetAttributeType(this ConfigBuilderOptions target, Type attribute)
+        public static IConfigBuilderOptions SetAttributeType(this IConfigBuilderOptions target, Type attribute)
         {
             if (attribute == null) throw new ArgumentNullException(nameof(attribute));
             target.SetupOptions(x => x.AttributeType = attribute);
             return target;
         }
 
-        public static IConfigBuilderOptions SetDateTimeFormat(this ConfigBuilderOptions target, string dateTimeFormat)
+        public static IConfigBuilderOptions SetDateTimeFormat(this IConfigBuilderOptions target, string dateTimeFormat)
         {
             target.SetupOptions(x => x.DateTimeFormat = dateTimeFormat);
             return target;
         }
 
-        public static IConfigBuilderOptions SetInterfaceBase(this ConfigBuilderOptions target, Type interfaceType)
+        public static IConfigBuilderOptions SetInterfaceBase(this IConfigBuilderOptions target, Type interfaceType)
         {
             target.SetupOptions(x => x.InterfaceBase = interfaceType);
             return target;
         }
 
-        public static ConfigBuilderOptions SetSectionNameFormatter(this ConfigBuilderOptions target, Func<Type, string> sectionNameFormatter)
+        public static IConfigBuilderOptions SetSectionNameFormatter(this IConfigBuilderOptions target, Func<Type, string> sectionNameFormatter)
         {
             target.SetupOptions(x => x.SectionNameFormatter = sectionNameFormatter);
             return target;
         }
 
-        public static IConfigBuilderOptions AddAssembly(this ConfigBuilderOptions target, Assembly assembly)
+        public static IConfigBuilderOptions AddAssembly(this IConfigBuilderOptions target, Assembly assembly)
         {
             target.AddAssemblies(new []{assembly});
             return target;
         }
 
-        public static IConfigBuilderOptions AddAssembly<T>(this ConfigBuilderOptions target)
+        public static IConfigBuilderOptions AddAssembly<T>(this IConfigBuilderOptions target)
         {
             var type = typeof(T);
             target.AddAssembly(type.Assembly);
             return target;
         }
         
-        public static IConfigBuilderOptions AddAssemblies(this ConfigBuilderOptions target, Assembly assembly, params Assembly[] assemblies)
+        public static IConfigBuilderOptions AddAssemblies(this IConfigBuilderOptions target, Assembly assembly, params Assembly[] assemblies)
         {
             assemblies = assemblies == null ? new [] {assembly} : assemblies.Concat(new []{ assembly }).ToArray();
             target.AddAssemblies(assemblies);
